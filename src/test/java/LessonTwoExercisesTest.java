a833390692db57f63385e0759778e224153fd1be
alexanderbogdanov <ironweed@list.ru>
2022-09-17 16:03:47 +0300

    ex 6 added

--- src/test/java/LessonTwoExercisesTest.java
+++ src/test/java/LessonTwoExercisesTest.java
@@ -1,4 +1,5 @@
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -13,5 +14,18 @@ public class LessonTwoExercisesTest {
     JsonPath response = given().get(url).jsonPath();
     ArrayList<String> messages = response.getJsonObject("messages.message");
     System.out.println(messages.get(1));
+  }
+
+    @Test
+  public void testFirstRedirectUrl() {
+    String url = "https://playground.learnqa.ru/api/long_redirect";
+    Response response = given()
+            .redirects()
+            .follow(false)
+            .when()
+            .get(url)
+            .andReturn();
+    String locationHeader = response.getHeader("Location");
+    System.out.println(locationHeader);
   }
 }
