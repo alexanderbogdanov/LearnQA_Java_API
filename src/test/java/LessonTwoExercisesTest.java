3a197fa9c37e69f8ec8bdac2797650663df1ae13
alexanderbogdanov <ironweed@list.ru>
2022-09-17 16:10:09 +0300

    ex 8 added

--- src/test/java/LessonTwoExercisesTest.java
+++ src/test/java/LessonTwoExercisesTest.java
@@ -2,9 +2,12 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class LessonTwoExercisesTest {
 
@@ -51,4 +54,50 @@ public class LessonTwoExercisesTest {
     System.out.println("Redirects: " + counter);
   }
 
+  @Test
+  public void tokensTest() throws InterruptedException {
+    String url = "https://playground.learnqa.ru/ajax/api/longtime_job";
+    Map<String, String> params = new HashMap<>();
+    List<Object> tokenAndTime = getTokenAndTime();
+    params.put("token", (String) tokenAndTime.get(1));
+
+    // check the status before the job is ready
+    given()
+            .queryParams(params)
+            .when()
+            .get(url)
+            .then()
+            .assertThat()
+            .body("status", equalTo("Job is NOT ready"));
+
+    waitForJob((Integer) tokenAndTime.get(0));
+
+    // check the status after the job is ready
+    given()
+            .queryParams(params)
+            .get(url)
+            .then()
+            .assertThat()
+            .body("status", equalTo("Job is ready"))
+            .body("result", notNullValue());
+  }
+
+  // create job and get the token
+  private List<Object> getTokenAndTime() {
+    String url = "https://playground.learnqa.ru/ajax/api/longtime_job";
+    JsonPath response =
+            given()
+                    .contentType("application/json")
+                    .when()
+                    .get(url)
+                    .jsonPath();
+    int seconds = response.get("seconds");
+    String token = response.get("token");
+    return Arrays.asList(seconds, token);
+  }
+
+  private void waitForJob(int seconds) throws InterruptedException {
+    System.out.println("Waiting for " + (seconds) + " seconds for job to be done");
+    TimeUnit.SECONDS.sleep(seconds);
+  }
 }
